ed02bc2a3ca36da6a13261d40cf64269802c7707
Tiafeno <tiafenofnel@gmail.com>
2018-07-26 22:17:35 +0300

    Implement brands logo in footer

--- footer.php
+++ footer.php
@@ -1,69 +1,25 @@
 <!-- footer -->
 <footer>
-
+<?php if (function_exists('get_field')): ?>
 	<div class="uk-section uk-section-large uk-padding-remove-vertical jp-brands">
 		<div class="uk-container uk-container-small uk-margin-top uk-margin-bottom">
 			<div class="jp-logo-brands">
 				<!-- sidebar register content footer -->
+				<?php
+				// @link https://www.advancedcustomfields.com/resources/gallery/
+				$brands = get_field('brands', 'option');
+				?>
 				<div class="uk-position-relative uk-visible-toggle uk-light"
 				     uk-slider="autoplay: true; autoplay-interval: 2500">
 
 					<ul class="uk-slider-items uk-child-width-1-3 uk-child-width-1-6@s uk-grid">
+						<?php foreach ($brands as $brand): ?>
 						<li>
 							<div class="uk-panel">
-								<img src="img/hyundi.png" alt="">
+								<img src="<?= $brand['sizes']['shop_catalog'] ?>" alt="">
 							</div>
 						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/kia.png" alt="">
-							</div>
-						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/renault.png" alt="">
-							</div>
-						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/seat.png" alt="">
-							</div>
-						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/hyundi.png" alt="">
-							</div>
-						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/kia.png" alt="">
-							</div>
-						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/renault.png" alt="">
-							</div>
-						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/seat.png" alt="">
-							</div>
-						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/kia.png" alt="">
-							</div>
-						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/renault.png" alt="">
-							</div>
-						</li>
-						<li>
-							<div class="uk-panel">
-								<img src="img/seat.png" alt="">
-							</div>
-						</li>
+						<?php endforeach; ?>
 
 					</ul>
 
@@ -76,32 +32,52 @@
 			</div>
 		</div>
 	</div>
+	<?php endif; ?>
 
 	<div class="uk-section uk-section-secondary jp-sitemap">
 		<div class="uk-container uk-container-small">
 			<div uk-grid>
 				<div class="uk-width-1-3@s uk-width-1-2">
-					<aside class="jp-aside-footer">
-						<h4 class="ui header">Service client</h4>
-						<div class="aside-content">
-							<ul class="uk-list">
-								<li><a href="#">Faq</a></li>
-								<li><a href="#">Nous contacter</a></li>
-							</ul>
-						</div>
-					</aside>
+					<?php if (has_nav_menu('left-footer')): ?>
+						<?php
+						$locations = get_nav_menu_locations();
+						$menu_id = $locations[ "left-footer" ] ;
+						$menu = wp_get_nav_menu_object($menu_id);
+						?>
+						<aside class="jp-aside-footer">
+
+							<h4 class="ui header"><?= $menu->name ?></h4>
+							<?php
+							wp_nav_menu( [
+								'menu_class' => "uk-list",
+								'theme_location' => 'left-footer',
+								'container_class' => 'aside-content',
+								'container' => 'div'
+							] );
+							?>
+						</aside>
+					<?php endif; ?>
 				</div>
 				<div class="uk-width-1-3@s uk-width-1-2">
+					<?php if (has_nav_menu('middle-footer')): ?>
+						<?php
+						$locations = get_nav_menu_locations();
+						$menu_id = $locations[ "middle-footer" ] ;
+						$menu = wp_get_nav_menu_object($menu_id);
+						?>
 					<aside class="jp-aside-footer">
-						<h4 class="ui header">Nos vehicules</h4>
-						<div class="aside-content">
-							<ul class="uk-list">
-								<li><a href="#">Les citadines</a></li>
-								<li><a href="#">Les véhicules utilitaires</a></li>
-								<li><a href="#">Les Suv & Crossovers</a></li>
-							</ul>
-						</div>
+
+						<h4 class="ui header"><?= $menu->name ?></h4>
+						<?php
+						wp_nav_menu( [
+							'menu_class' => "uk-list",
+							'theme_location' => 'middle-footer',
+							'container_class' => 'aside-content',
+							'container' => 'div'
+						] );
+						?>
 					</aside>
+					<?php endif; ?>
 				</div>
 				<div class="uk-width-1-3@s uk-width-1-2">
 					<aside class="jp-aside-footer">

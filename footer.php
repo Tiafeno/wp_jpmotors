053977d6b5bbdac694e6427990ae72983e0f2665
Tiafeno <tiafenofnel@gmail.com>
2018-08-23 22:26:08 +0300

    Added brand logo an url

--- footer.php
+++ footer.php
@@ -1,25 +1,32 @@
 <!-- footer -->
 <footer>
-<?php if (function_exists('get_field')): ?>
+<?php if (function_exists('get_field')) : ?>
 	<div class="uk-section uk-section-large uk-padding-remove-vertical jp-brands">
 		<div class="uk-container uk-container-small uk-margin-top uk-margin-bottom">
 			<div class="jp-logo-brands">
 				<!-- sidebar register content footer -->
 				<?php
 				// @link https://www.advancedcustomfields.com/resources/gallery/
-				$brands = get_field('brands', 'option');
-				?>
+			$brands = get_field('brands', 'option');
+			?>
 				<div class="uk-position-relative uk-visible-toggle uk-light"
 				     uk-slider="autoplay: true; autoplay-interval: 2500">
 
 					<ul class="uk-slider-items uk-child-width-1-3 uk-child-width-1-6@s uk-grid">
-						<?php foreach ($brands as $brand): ?>
+						<?php 
+					foreach ($brands as $brand) :
+						$link = get_field('link_constructor', $brand['ID']);
+					?>
 						<li>
 							<div class="uk-panel">
-								<img src="<?= $brand['sizes']['medium_large'] ?>" alt="">
+								<a href="<?= $link ? $link : '#no_link' ?>">
+									<img src="<?= $brand['sizes']['medium_large'] ?>" alt="">
+								</a>
 							</div>
 						</li>
-						<?php endforeach; ?>
+						<?php 
+					endforeach;
+					?>
 
 					</ul>
 
@@ -38,44 +45,44 @@
 		<div class="uk-container uk-container-small">
 			<div uk-grid>
 				<div class="uk-width-1-3@s uk-width-1-2">
-					<?php if (has_nav_menu('left-footer')): ?>
+					<?php if (has_nav_menu('left-footer')) : ?>
 						<?php
-						$locations = get_nav_menu_locations();
-						$menu_id = $locations[ "left-footer" ] ;
-						$menu = wp_get_nav_menu_object($menu_id);
-						?>
+					$locations = get_nav_menu_locations();
+					$menu_id = $locations["left-footer"];
+					$menu = wp_get_nav_menu_object($menu_id);
+					?>
 						<aside class="jp-aside-footer">
 
 							<h4 class="ui header"><?= $menu->name ?></h4>
 							<?php
-							wp_nav_menu( [
-								'menu_class' => "uk-list",
-								'theme_location' => 'left-footer',
-								'container_class' => 'aside-content',
-								'container' => 'div'
-							] );
-							?>
+						wp_nav_menu([
+							'menu_class' => "uk-list",
+							'theme_location' => 'left-footer',
+							'container_class' => 'aside-content',
+							'container' => 'div'
+						]);
+						?>
 						</aside>
 					<?php endif; ?>
 				</div>
 				<div class="uk-width-1-3@s uk-width-1-2">
-					<?php if (has_nav_menu('middle-footer')): ?>
+					<?php if (has_nav_menu('middle-footer')) : ?>
 						<?php
-						$locations = get_nav_menu_locations();
-						$menu_id = $locations[ "middle-footer" ] ;
-						$menu = wp_get_nav_menu_object($menu_id);
-						?>
+					$locations = get_nav_menu_locations();
+					$menu_id = $locations["middle-footer"];
+					$menu = wp_get_nav_menu_object($menu_id);
+					?>
 					<aside class="jp-aside-footer">
 
 						<h4 class="ui header"><?= $menu->name ?></h4>
 						<?php
-						wp_nav_menu( [
-							'menu_class' => "uk-list",
-							'theme_location' => 'middle-footer',
-							'container_class' => 'aside-content',
-							'container' => 'div'
-						] );
-						?>
+					wp_nav_menu([
+						'menu_class' => "uk-list",
+						'theme_location' => 'middle-footer',
+						'container_class' => 'aside-content',
+						'container' => 'div'
+					]);
+					?>
 					</aside>
 					<?php endif; ?>
 				</div>
